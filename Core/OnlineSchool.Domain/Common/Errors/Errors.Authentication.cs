182f552d788d544b13ff3e2e359ac814f938a4f5
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-02-21 11:49:16 +0300

    Создание возможных ошибок

---
+++ Core/OnlineSchool.Domain/Common/Errors/Errors.Authentication.cs
@@ -0,0 +1,18 @@
+﻿using ErrorOr;
+
+namespace OnlineSchool.Domain.Common.Errors
+{
+    public static partial class Errors
+    {
+        public static class Authentication
+        {
+            public static Error InvalidCredentials => Error.Validation(
+                code: "Auth.InvalidCred",
+                description: "Invalid credentials.");
+
+            public static Error DuplicateEmail => Error.Validation(
+                code: "Auth.DupEmail",
+                description: "This user already exists");
+        }
+    }
+}
