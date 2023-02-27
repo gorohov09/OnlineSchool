3390fd3f82dc9c8f391303414817cbb9308b154a
Damir <damiros.20002@yandex.ru>
2023-02-27 19:55:51 +0300

    Работая над задачей 8. Разрабатываю класс GetStudentCoursesModulesVM

---
+++ Core/OnlineSchool.App/Student/Queries/GetFullCourses/GetStudentCoursesModulesVm.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineSchool.App.Student.Queries.GetFullCourses
+{
+    public record StudentCoursesVm(
+
+        );
+    public record CourseVm(
+        Guid ID,
+        string Name,
+        List<ModuleVm> Modules
+        );
+    public record ModuleVm(
+        Guid ID,
+        string Name,
+        List<LessonVm> Lessons
+        );
+    public record LessonVm(
+        Guid ID,
+        string Name
+        );
+}
