f2aa9b0e9eb3340abafa397b3ec54c354db75c71
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-03-01 12:17:37 +0300

    Создание необходимых интерфейсов

--- Core/OnlineSchool.App/Common/Interfaces/Persistence/IStudentRepository.cs
+++ Core/OnlineSchool.App/Common/Interfaces/Persistence/IStudentRepository.cs
@@ -3,7 +3,7 @@ using OnlineSchool.Domain.Student;
 
 namespace OnlineSchool.App.Common.Interfaces.Persistence;
 
-public interface IStudentRepository
+public interface IStudentRepository : IGenericRepository<StudentEntity>
 {
     Task AddStudent(StudentEntity student);
     Task<List<InformationAdmissionEntity>> GetInformationAdmissions(Guid studentId);
---
+++ Core/OnlineSchool.App/Common/Interfaces/Persistence/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineSchool.App.Common.Interfaces.Persistence;
+
+public interface IUnitOfWork
+{
+    IStudentRepository Users { get; }
+
+    Task CompleteAsync();
+}
