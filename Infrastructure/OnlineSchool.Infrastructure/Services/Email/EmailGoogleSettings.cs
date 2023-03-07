a4549f5c506cd579e672feb265dc863123799846
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-07 04:02:53 +0300

    Создание настроек для собственного Gmail

---
+++ Infrastructure/OnlineSchool.Infrastructure/Services/Email/EmailGoogleSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineSchool.Infrastructure.Services.Email
+{
+	public class EmailGoogleSettings
+	{
+		public const string SectionName = "EmailSettings";
+
+		public string Address { get; init; } = null!;
+		public string GmailPassword { get; init; } = null!;
+		public string From { get; init; } = null!;
+
+	}
+}
