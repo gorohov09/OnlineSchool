9949a8f828dc4af7f12a04f4490e29f157d3a138
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-11 00:17:03 +0300

    Модель отправки полной информации о курсе

---
+++ Presentation/OnlineSchool.Contracts/Course/Get/GetCourseDetailsResponse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineSchool.Contracts.Course.Get
+{
+	public record GetCourseDetailsResponse(
+		string CourseId,
+		string Name,
+		string Description,
+		DateTime Create,
+		DateTime Update,
+		List<ModuleResponse> Modules);
+
+	public record ModuleResponse(
+		string ModuleId,
+		string Name,
+		int Order,
+		List<LessonResponse> Lessons);
+
+	public record LessonResponse(
+		string LessonId,
+		int Order,
+		string Name);
+		
+
+
+
+
+
+
+
+
+}
