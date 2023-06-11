0e58c92849dbb24ee358bc644595316ce8757de3
Андрей Горохов <andrej.gorokhoff2017@yandex.ru>
2023-06-11 19:57:44 +0300

    Добавление клиента

---
+++ Presentation/OnlineSchool.API/ClientApp/src/components/loginForm/LoginForm.jsx
@@ -0,0 +1,84 @@
+import Button from '@mui/material/Button';
+
+import {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import useCourseService from '../../services/CourseService.jsx';
+import { ThemeProvider  } from '@mui/material/styles';
+import theme from '../muiTheme.jsx';
+
+import './loginForm.scss'
+
+const LoginForm = ({setToken, setIsAuth, setIsManager}) => {
+    const {loginUser, error, clearError} = useCourseService();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+		e.preventDefault();
+        
+		const data = await loginUser({
+			email: email,
+		  	password: password
+		});
+        console.log(data);
+		if (data?.status === 500){
+			console.log('Очистка формы')
+			e.target.reset(); 
+		}
+		else{
+			setToken(data.token);
+            console.log(data.token);
+			setIsAuth(true);
+            console.log(data.typeUser);
+			if (data.typeUser === 'teacher')
+				navigate('/');
+			else if (data.typeUser === 'student')
+				navigate('/student');
+            else 
+                navigate('/manager');
+		}
+		
+	}
+
+    useEffect(() => {
+        clearError();
+    }, []);
+
+    let errorMessage = (
+        <div>
+            <span style={{'color': '#ffffff', 'font-size': '1em'}}>
+                Произошла ошибка
+            </span>
+        </div>
+    )
+    errorMessage = error ? errorMessage : null;
+
+    return(
+        <div className="form login_form">
+            <form onSubmit={handleSubmit}> 
+                <div className="email input">
+                    <label>
+                        <p>Почта</p>
+                        <input type="text" onChange={e => setEmail(e.target.value)}/>
+                    </label>
+                </div>
+                <div className="password input">
+                    <label>
+                        <p>Пароль</p>
+                        <input type="password" onChange={e => setPassword(e.target.value)}/>
+                    </label>
+                </div>
+                <div className="button input">
+                    <ThemeProvider theme={theme}>
+                        <Button variant="contained" size="medium" type="submit">Авторизация</Button>
+                    </ThemeProvider>
+                </div>     
+                {errorMessage}
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
